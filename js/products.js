f337f9efe333824e32b61afe36719f6d5ab1337b
filipjt88 <filip88ks@gmail.com>
2025-07-12 22:13:04 +0200

    add products

---
+++ js/products.js
@@ -0,0 +1,6 @@
+const products = [
+    { id: 1, name: "iPhone 14", price: 999, image: "images/iphone14.jpg", description: "The iPhone 14 is a smartphone with a 6.1-inch Super Retina XDR display, a dual-camera system, and the A15 Bionic chip. It offers advanced camera features, including improved low-light performance, Action mode for smooth video, and Cinematic mode in 4K." },
+    { id: 2, name: "Samsung Galaxy S23", price: 899, image: "images/s23.jpg", description: "The Samsung Galaxy S23 series, released on February 17, 2023, features three models: the S23, S23+, and S23 Ultra. All models share a similar design language with the S22 series but introduce upgraded processors, cameras, and batteries." },
+    { id: 3, name: "Xiaomi 13 Pro", price: 749, image: "images/xaomi13pro.jpg", description: "The Xiaomi 13 Pro is a high-end smartphone featuring a Leica professional optical lens system, a 6.73-inch WQHD+ AMOLED display with a 120Hz refresh rate, and a 4820mAh battery with 120W HyperCharge capabilities." },
+    { id: 3, name: "Honor 90", price: 550, image: "images/honor-90.jpg", description: "The Honor 90 is a mid-range smartphone featuring a 200MP ultra-clear main camera, a 6.7-inch AMOLED quad-curved display with a 120Hz refresh rate, and a Snapdragon 7 Gen 1 Accelerated Edition processor." },
+];
